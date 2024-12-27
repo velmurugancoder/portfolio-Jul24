6227eabd7d10c8b48a0706d932193b2676d577d7
P.velmurugan <velmurugancoder@gmail.com>
2024-12-27 20:55:27 +0530

    first host commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  transpileDependencies: ["vuetify"],
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio-Jul24/" : "/",
+  homepage: "https://velmurugancoder.github.io/portfolio-Jul24",
+});
